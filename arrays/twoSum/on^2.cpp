095cd9d3d9d4b23158f0057a96c177a85f5dcb9e
Isaac Reynaldo <reyisaac93@hotmail.com>
2018-09-08 10:53:47 -0400

    array n^2 and nlogn

---
+++ arrays/twoSum/on^2.cpp
@@ -0,0 +1,9 @@
+#include <iostrean>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
---
+++ interviewQuestions/interviewQuestions/Solutions.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solutions {
+public:
+	//ARRAYS
+	//two sums O(n^2)
+	vector<int> twosums(vector<int>& numList, int target) {
+		int n = numList.size();
+		for (int i = 0; i < n; ++i)
+		{
+			for (int k = 0; k < n; ++k) {
+				cout << "test" << endl;
+				if (i != k)
+				{
+					if (numList[i] + numList[k] == target) {
+						cout << "[" << i << "]["<< k << "]"<< endl;
+						vector<int> temp{ i, k };
+						return temp;
+					}
+				}
+			}
+		}
+	}
+	//two sums O(n log n)
+	vector<int> twosums_optimized(vector<int>& numList, int target) {
+		//use pair for creating a temp array
+		vector<pair<int, int>> nums2;
+		for (int i = 0; i < numList.size(); ++i) {
+			pair<int, int> t(numList[i], i);
+			nums2.push_back(t);
+		}
+
+		//use nums 2 from now on
+		sort(nums2.begin(), nums2.end()); //first step sort the list
+		int i = 0;
+		int k = nums2.size() - 1;
+		while (i > k) {
+			if (nums2[i].first + nums2[k].first == target) {
+				cout << "[" << i << "][" << k << "]" << endl;
+				vector<int> temp{ nums2[i].second, nums2[k].second };
+				return temp;
+			}
+			else if (nums2[i].first + nums2[k].first) --k;
+			else ++i;
+		}
+
+	}
+};
